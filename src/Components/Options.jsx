754b67df498779c8ead2cb63cc6a2178f70c802b
Carlos Aparecido <90780691+ImCarL6@users.noreply.github.com>
2023-08-22 11:38:48 -0300

    feat: added animation and style to buttons

--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -15,6 +15,8 @@ export const Options = () => {
   const [theme, setTheme] = useState(lsTheme || "light");
   const [icon, setIcon] = useState(lsIcon || "bx-moon");
   const [snow, setSnow] = useState(lsSnow);
+  const [shouldResetThemeAnimation, setShouldResetThemeAnimation] = useState(false);
+  const [shouldResetSnowAnimation, setShouldResetSnowAnimation] = useState(false);
 
   useEffect(() => {
     localStorage.setItem("theme", theme);
@@ -26,17 +28,33 @@ export const Options = () => {
   const SnowEffect = () =>
     snow && theme === "dark" && <Particles params={ParticlesParams} />;
 
-  const _enableSnow = () => setSnow(!snow);
+  const _enableSnow = () => {
+    setSnow(!snow);
+    setShouldResetSnowAnimation(true);
+
+    // Reset animation after a short delay
+    setTimeout(() => {
+      setShouldResetSnowAnimation(false);
+    }, 50);
+  };
   const _toggleTheme = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
     icon === "bx-sun" ? setIcon("bx-moon") : setIcon("bx-sun");
+    setShouldResetThemeAnimation(true);
+
+    // Reset animation after a short delay
+    setTimeout(() => {
+      setShouldResetThemeAnimation(false);
+    }, 50);
   };
 
   return (
     <div className="home__options">
       {theme === "dark" && (
         <i
-          className="bx bx-cloud-snow enable-snow"
+          className={`bx bx-cloud-snow enable-snow animated-button pulse-aura button-indicator ${
+            shouldResetSnowAnimation ? "reset-animation" : ""
+          }`}
           title="Activate Snow"
           id="snow-button"
           onClick={_enableSnow}
@@ -44,7 +62,9 @@ export const Options = () => {
       )}
       <SnowEffect />
       <i
-        className={`bx ${icon} change-theme`}
+        className={`bx ${icon} change-theme animated-button pulse-aura button-indicator ${
+          shouldResetThemeAnimation ? "reset-animation" : ""
+        }`}
         title="Toggle Theme"
         id="theme-button"
         onClick={_toggleTheme}
