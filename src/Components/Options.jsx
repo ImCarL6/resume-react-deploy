b01f0b6c2df31ef3651d9136bb53d35d32aeaeac
Carlos Aparecido <90780691+ImCarL6@users.noreply.github.com>
2023-08-22 12:13:18 -0300

    feat: site now starts in dark mode

--- src/Components/Options.jsx
+++ src/Components/Options.jsx
@@ -3,49 +3,38 @@ import Particles from "react-particles-js";
 import { ParticlesParams } from "../Schemas/Particles";
 
 export const Options = () => {
-  var { lsTheme, lsIcon, lsSnow } = "";
+  var { lsIcon } = "";
   try {
-    lsTheme = localStorage.getItem("theme");
     lsIcon = localStorage.getItem("icon");
-    lsSnow = JSON.parse(localStorage.getItem("particles"));
   } catch (e) {
     console.error(`All Cookies blocked - Error: ${e.message}`);
   }
 
-  const [theme, setTheme] = useState(lsTheme || "light");
   const [icon, setIcon] = useState(lsIcon || "bx-moon");
-  const [snow, setSnow] = useState(lsSnow);
-  const [shouldResetThemeAnimation, setShouldResetThemeAnimation] = useState(false);
-  const [shouldResetSnowAnimation, setShouldResetSnowAnimation] = useState(false);
+  const [theme, setTheme] = useState("dark");
+  const [snow, setSnow] = useState(true);
+  const [snowAnimation, setSnowAnimation] = useState(false);
 
   useEffect(() => {
-    localStorage.setItem("theme", theme);
     localStorage.setItem("icon", icon);
-    localStorage.setItem("snow", snow);
     document.body.classList[theme === "dark" ? "add" : "remove"]("dark-theme");
-  }, [theme, snow, icon]);
+  }, [theme, icon]);
 
   const SnowEffect = () =>
-    snow && theme === "dark" && <Particles params={ParticlesParams} />;
+  snow && theme === "dark" && <Particles params={ParticlesParams} />;
 
-  const _enableSnow = () => {
+  const toggleSnow = () => {
     setSnow(!snow);
-    setShouldResetSnowAnimation(true);
-
-    // Reset animation after a short delay
-    setTimeout(() => {
-      setShouldResetSnowAnimation(false);
-    }, 50);
   };
-  const _toggleTheme = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-    icon === "bx-sun" ? setIcon("bx-moon") : setIcon("bx-sun");
-    setShouldResetThemeAnimation(true);
 
-    // Reset animation after a short delay
-    setTimeout(() => {
-      setShouldResetThemeAnimation(false);
-    }, 50);
+  const _toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+    setIcon(icon === "bx-sun" ? "bx-moon" : "bx-sun");
+    setSnowAnimation(false);
+
+    if (theme === "dark") {
+      setSnow(false);
+    }
   };
 
   return (
@@ -53,22 +42,22 @@ export const Options = () => {
       {theme === "dark" && (
         <i
           className={`bx bx-cloud-snow enable-snow animated-button pulse-aura button-indicator ${
-            shouldResetSnowAnimation ? "reset-animation" : ""
+            snow ? "reset-animation" : ""
           }`}
           title="Activate Snow"
           id="snow-button"
-          onClick={_enableSnow}
+          onClick={toggleSnow}
         />
       )}
       <SnowEffect />
       <i
-        className={`bx ${icon} change-theme animated-button pulse-aura button-indicator ${
-          shouldResetThemeAnimation ? "reset-animation" : ""
-        }`}
-        title="Toggle Theme"
-        id="theme-button"
-        onClick={_toggleTheme}
-      />
+  className={`bx ${icon} change-theme animated-button pulse-aura button-indicator start-animation ${
+    snowAnimation ? "reset-animation" : ""
+  }`}
+  title="Toggle Theme"
+  id="theme-button"
+  onClick={_toggleTheme}
+/>
     </div>
   );
 };
