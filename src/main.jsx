c67cbafa2cb8861bf864dda0f9d5e3b7b51eb70a
Carlos Aparecido <90780691+ImCarL6@users.noreply.github.com>
2023-08-24 15:59:26 -0300

    removed unused import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom";
-import config from '../config.json'
 
 import "./App.css";
 
