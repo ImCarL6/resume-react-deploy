3b01a4aa7160c533f60a5448a4c873b8412a8fbe
Carlos Aparecido <90780691+ImCarL6@users.noreply.github.com>
2023-08-24 15:58:25 -0300

    added vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { createRoot } from "react-dom/client"
-import { analytics } from '@vercel/analytics';
 import { BrowserRouter } from "react-router-dom";
 import config from '../config.json'
 
@@ -8,9 +7,9 @@ import "./App.css";
 
 import { App } from "./Components/App";
 
-analytics({
-  projectId: config.VERCEL_PROJECT_ID,
-});
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
