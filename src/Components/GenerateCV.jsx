f0c48e93054207cc93f69a89b4a3a1fe67d610cb
Carlos Aparecido <90780691+ImCarL6@users.noreply.github.com>
2023-08-28 12:02:56 -0300

    feat: added generation of pdf in mobile view

--- src/Components/GenerateCV.jsx
+++ src/Components/GenerateCV.jsx
@@ -4,72 +4,97 @@ import jsPDF from "jspdf";
 
 import { POPPINS_FONT_B64 } from "../Utils/Constants";
 
-export const GenerateCV = ({ email, location, telephone, social }) => {
+export const GenerateCV = ({ email, location, telephone, social, isMobileView }) => {
   const [infoNodes, setInfoNodes] = useState([]);
   const [infoNodesText, setInfoNodesText] = useState([]);
   const [socialNodes, setSocialNodes] = useState([]);
   const [socialNodesText, setSocialNodesText] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [downloadUrl, setDownloadUrl] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
   const generateResume = async () => {
-    const originalViewportWidth = window.innerWidth;
-    const originalViewportHeight = window.innerHeight;
-
-    window.innerWidth = 1366;
-    window.innerHeight = 768;
-
-    prepareResume();
-
     const currentResourceURL = window.location.pathname;
     const isBRPage = currentResourceURL.includes("/br");
 
-    const rootCV = document.getElementById("area-cv");
-    const canvasCV = await html2canvas(rootCV).catch((e) => console.error(e));
-    const imageCV = canvasCV.toDataURL("image/png");
+    if (isMobileView){
+      setIsLoading(true)
 
-    let pdf;
-    let starterPositionY;
-
-    if (isBRPage) {
-      pdf = new jsPDF({ format: [450, 255] });
-      starterPositionY = 436;
+      const response = await fetch('https://carlos-cv-generator.cyclic.app/generate-pdf')
+      const fileUrl = await response.text()
+  
+      try {
+        setDownloadUrl(fileUrl);
+        window.location.href = fileUrl;
+      } catch (error) {
+        console.error('Error getting signed URL:', error);
+      }
+  
+      setIsLoading(false);
+      setSuccessMessage('Successfully generated!');
+      setTimeout(() => {
+        setSuccessMessage('');
+      }, 5500);  
     } else {
-      pdf = new jsPDF({ format: [430, 255] });
-      starterPositionY = 418;
-    }
 
-    window.innerWidth = originalViewportWidth;
-    window.innerHeight = originalViewportHeight;
-
-    pdf.addImage(imageCV, "PNG", 0, 0);
-    pdf.addFileToVFS("poppins-Medium.ttf", POPPINS_FONT_B64);
-    pdf.addFont("poppins-Medium.ttf", "POPPINS_FONT_B64", "normal");
-    pdf.setFont("POPPINS_FONT_B64");
-    pdf.setTextColor(
-      document.body.classList.contains("dark-theme") ? "#b5c4db" : "#4c566a"
-    );
-    pdf.setFontSize(11);
-    pdf.text(location, 15, 72);
-    pdf.text(email, 15, 82);
-    pdf.text(telephone, 15, 92);
-
-    const starterPositionX = 15;
-    const sectionTitleElement = document.querySelector(
-      ".social.section .section-title"
-    );
-
-    if (sectionTitleElement) {
-      social.forEach((socialProp) => {
-        pdf.textWithLink(socialProp.label, starterPositionX, starterPositionY, {
-          url: socialProp.url,
+      const originalViewportWidth = window.innerWidth;
+      const originalViewportHeight = window.innerHeight;
+  
+      window.innerWidth = 1366;
+      window.innerHeight = 768;
+  
+      prepareResume();
+  
+      const rootCV = document.getElementById("area-cv");
+      const canvasCV = await html2canvas(rootCV).catch((e) => console.error(e));
+      const imageCV = canvasCV.toDataURL("image/png");
+  
+      let pdf;
+      let starterPositionY;
+  
+      if (isBRPage) {
+        pdf = new jsPDF({ format: [450, 255] });
+        starterPositionY = 436;
+      } else {
+        pdf = new jsPDF({ format: [430, 255] });
+        starterPositionY = 418;
+      }
+  
+      window.innerWidth = originalViewportWidth;
+      window.innerHeight = originalViewportHeight;
+  
+      pdf.addImage(imageCV, "PNG", 0, 0);
+      pdf.addFileToVFS("poppins-Medium.ttf", POPPINS_FONT_B64);
+      pdf.addFont("poppins-Medium.ttf", "POPPINS_FONT_B64", "normal");
+      pdf.setFont("POPPINS_FONT_B64");
+      pdf.setTextColor(
+        document.body.classList.contains("dark-theme") ? "#b5c4db" : "#4c566a"
+      );
+      pdf.setFontSize(11);
+      pdf.text(location, 15, 72);
+      pdf.text(email, 15, 82);
+      pdf.text(telephone, 15, 92);
+  
+      const starterPositionX = 15;
+      const sectionTitleElement = document.querySelector(
+        ".social.section .section-title"
+      );
+  
+      if (sectionTitleElement) {
+        social.forEach((socialProp) => {
+          pdf.textWithLink(socialProp.label, starterPositionX, starterPositionY, {
+            url: socialProp.url,
+          });
+  
+          starterPositionY += 9;
         });
-
-        starterPositionY += 9;
-      });
+      }
+  
+      rollbackResume();
+  
+      pdf.save("resume.pdf");
     }
 
-    rollbackResume();
-
-    pdf.save("resume.pdf");
   };
 
   const prepareResume = () => {
@@ -112,9 +137,15 @@ export const GenerateCV = ({ email, location, telephone, social }) => {
 
   return (
     <div className="generate__container" id="resume__generate">
-      <button href="#" className="generate__btn" onClick={generateResume}>
-        Download CV
+      <button
+        href="#"
+        className="generate__btn"
+        onClick={generateResume}
+        disabled={isLoading}
+      >
+        {isLoading ? "Generating resume..." : "Download CV"}
       </button>
+      {successMessage && <p className="success-message">{successMessage}</p>}
     </div>
   );
 };
